d998d10d3234e0c3a1d82f03ee8eace351477c6f
aspartax42 <59195908+aspartax42@users.noreply.github.com>
2020-10-09 00:51:00 +0200

    Update zabbix_server.sh

--- zabbix_server.sh
+++ zabbix_server.sh
@@ -8,6 +8,7 @@ fi
 # Principaux paramètres
 tput setaf 7; read -p "Entrer le mot de passe root de la base de données: " ROOT_DB_PASS
 tput setaf 7; read -p "Entrer le mot de passe zabbix de la base de données: " ZABBIX_DB_PASS
+tput setaf 7; read -p "Entrer l'adresse IP du serveur Zabbix: " IP
 
 tput setaf 2; echo ""
 
@@ -54,6 +55,21 @@ mysql -uroot -p'$ROOT_DB_PASS' -D zabbix -e "set global innodb_strict_mode='ON';
 
 
 # Ajout du mot de passe Zabbix dans le fichier de conf
+echo "StartPollers=100
+StartPollersUnreachable=50
+StartPingers=50
+StartTrappers=10
+StartDiscoverers=15
+StartPreprocessors=15
+StartHTTPPollers=5
+StartAlerters=5
+StartTimers=2
+StartEscalators=2
+CacheSize=128M
+HistoryCacheSize=64M
+HistoryIndexCacheSize=32M
+TrendCacheSize=32M
+ValueCacheSize=256M" >> /etc/zabbix/zabbix_server.conf
 echo "DBPassword="$ZABBIX_DB_PASS"" >> /etc/zabbix/zabbix_server.conf
 
 systemctl restart zabbix-server zabbix-agent 
@@ -67,13 +83,13 @@ echo "php_value date.timezone Europe/Paris" >> /etc/zabbix/apache.conf
 systemctl restart apache2
 systemctl enable apache2
 
-ip = ip a
 
 clear
 echo "-------------------------------------------------"
 tput bold; tput setaf 7; echo "       => Installation terminée <=       "
 tput setaf 7; echo ""
-tput setaf 7; echo "URL du serveur Zabbix: http://"$ip"/zabbix"
+tput setaf 7; echo "URL du serveur Zabbix: http://"$IP"/zabbix"
+tput setaf 7; echo "Login:Admin / MDP: zabbix"
 tput setaf 7; echo ""
 
 
